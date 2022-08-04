fdf44f0c2180cb5fc381ee8d4201d3b82c9f4200
Leonardo Pualinggi <leonardo.pualingi@gmail.com>
2022-08-04 18:46:54 +0800

    konfigurasi #4

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const em = require("./src/events");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/mahasiswa", require("./src/routes/mahasiswa-route"));
 app.all("*", (req, res)=>{
@@ -14,6 +16,7 @@ app.all("*", (req, res)=>{
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log("Server siap!"));
+
 em.on("err", (msg)=>{
     console.log(msg);
     process.exit(0);
